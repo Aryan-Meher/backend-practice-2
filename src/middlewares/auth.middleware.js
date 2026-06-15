7ff0144bb7c42f57b49417b725a8280c433dc9e3
Aryan Meher <aryanmeher4242@gmail.com>
2026-06-16 01:18:43 +0530

    fix bugs

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -24,7 +24,7 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     req.user = user;
     next()
     } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token"
+        throw new ApiError(401, error?.message || "Invalid access token")
     }
 
 })
